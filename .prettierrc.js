5b2a6220c671e84ca086d811b262266a2fd25782
satoshionoda <satoshionoda@yohak.design>
2024-08-30 11:46:04 +0900

    wip adding components

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  tabWidth: 2,
+  printWidth: 100,
+};
+export default config;
